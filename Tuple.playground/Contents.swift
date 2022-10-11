fac5d1c7529d1f8816dd7b23227b1736f38a2350
vivek <vivekrjk33@gmail.com>
2022-10-11 13:14:12 +0530

    done

---
+++ Tuple.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// TTTTTTUUUUUUPPPPPPPLLLLLLLLEEEEEEEE(Tuple)
+
+
+
+func isvalidUsername(username: String) -> (errorMessage : String?, isValid : Bool) {
+    
+    if username.isEmpty {
+        return ("Username is empty", false)
+    }
+    else{
+        
+        return (nil, true)
+    }
+    
+}
+
+
+let isValidUsername1 = isvalidUsername(username: "")
+print("isValidName is" , isValidUsername1)
+print(isValidUsername1.errorMessage!)
+print(isValidUsername1.isValid)
+
+let isValidUserNmae2 = isvalidUsername(username: "Vivek")
+print("isValidUsername2", isValidUserNmae2)
+
+
+
+func isvalidPassword(password: String) -> (errorMessage : String?, isValid : Bool) {
+    
+    if password.count < 3 {
+        return ("password is too short" , false )
+    }
+    else if password.count > 20 {
+        return ("password is too long", false)
+    }
+    
+    else {
+        return ("valid password", true)
+    }
+}
+
+
+let password = isvalidPassword(password: "12")
+print("password", password)
+print("password", password.errorMessage!)
+
+
+let password2 = isvalidPassword(password: "vivisdvxhyvkfshvjksfhkshfvjfviv")
+print("password", password2.errorMessage!)
